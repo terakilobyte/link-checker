85043886b02682bd8ee7000b195d7a833c557138
Nathan Leniz <nathan.leniz@mongodb.com>
2021-09-30 12:52:42 -0700

    wrap first run in a timeout

--- src/extension.ts
+++ src/extension.ts
@@ -31,7 +31,9 @@ export function activate(context: vscode.ExtensionContext) {
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "link-checker" is now active!');
 
-  linkCheck();
+  setTimeout(() => {
+    linkCheck();
+  }, 5000);
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
